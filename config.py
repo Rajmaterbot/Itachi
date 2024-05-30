3d2d37635768854c7e11c9eef12b41cc4c4645fc
Raj Saini <98858458+Rajmaterbot@users.noreply.github.com>
2024-05-30 09:24:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7129448604:AAHBtE1PZG2YGutDVNZ_pgH5E_1UNW9BsDo")
-    API_ID = int(os.environ["API_ID", 23291931]
-    API_HASH = os.environ["API_HASH", "4b11dd648188731fb7c9bc8083e8791c"]
-    AUTH_USERS = "6594402123"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6441036103:AAHLmhHSMOyFS09mcBIqE5HP8_2LPaCWrkA")
+    API_ID = int(os.environ["API_ID", 28308147]
+    API_HASH = os.environ["API_HASH", "8240cdc945560ccd108b6ff2925ced5e"]
+    AUTH_USERS = "5618212564"
 # 
